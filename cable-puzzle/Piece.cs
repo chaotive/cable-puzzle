1ba2f4dc39abcedfc84b74c7148af15b982fa7b4
Leonardo Montenegro <leonardo.montenegro@chaotive.cl>
2017-02-22 23:48:13 -0300

    Defining base Piece type

---
+++ cable-puzzle/Piece.cs
@@ -0,0 +1,16 @@
+﻿namespace cable_puzzle
+{
+    public enum Orientation
+    {
+        Right,
+        Top,
+        Left,
+        Down
+    }
+
+    public class Piece
+    {
+        public string type;
+        public Orientation orientation;
+    }
+}
