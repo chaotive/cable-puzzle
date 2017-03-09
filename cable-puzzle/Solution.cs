4d45239a065020b3c834f6ab9ce652a6df44ff55
Leonardo Montenegro <leonardo.montenegro@chaotive.cl>
2017-03-09 00:13:38 -0300

    All 9 puzzles implemented

--- cable-puzzle/Solution.cs
+++ cable-puzzle/Solution.cs
@@ -23,7 +23,7 @@ namespace cable_puzzle
             new Solution(
                 new List<string> {
                     "00", "11", "10",
-                    "11", "12", "01",
+                    "10", "12", "01",
                     "13", "00", "12"
                 },
                 new List<int> {
@@ -33,19 +33,18 @@ namespace cable_puzzle
                 },
                 new Pins(PinPosition.Up, PinPosition.Up)
             ),
-            /*new Solution(
+            new Solution(
                 new List<string> {
-                    "00", "00", "11",
-                    "10", "00", "12",
+                    "11", "10", "11",
+                    "13", "12", "13",
                     "13", "00", "00"
                 },
                 new List<int> {
-                    0, 1, 2,
-                    5, 4, 3,
-                    6, 7, 8
+                    0, 3, 4,
+                    1, 2, 5
                 },
                 new Pins(PinPosition.Up, PinPosition.Middle)
-            ),*/
+            ),
             new Solution(
                 new List<string> {
                     "00", "00", "11",
@@ -58,6 +57,82 @@ namespace cable_puzzle
                     6, 7, 8
                 },
                 new Pins(PinPosition.Up, PinPosition.Down)
+            ),
+
+            new Solution(
+                new List<string> {
+                    "10", "11", "10",
+                    "12", "01", "01",
+                    "13", "13", "12"
+                },
+                new List<int> {
+                    3, 0, 1,
+                    4, 7, 8,
+                    5, 2
+                },
+                new Pins(PinPosition.Middle, PinPosition.Up)
+            ),
+            new Solution(
+                new List<string> {
+                    "11", "10", "11",
+                    "00", "11", "10",
+                    "13", "13", "12"
+                },
+                new List<int> {
+                    3, 4, 7,
+                    8, 5
+                },
+                new Pins(PinPosition.Middle, PinPosition.Middle)
+            ),
+            new Solution(
+                new List<string> {
+                    "10", "10", "11",
+                    "00", "12", "01",
+                    "13", "00", "13"
+                },
+                new List<int> {
+                    3, 4, 1,
+                    2, 5, 8
+                },
+                new Pins(PinPosition.Middle, PinPosition.Down)
+            ),
+            new Solution(
+                new List<string> {
+                    "10", "00", "00",
+                    "13", "11", "01",
+                    "00", "12", "12"
+                },
+                new List<int> {
+                    6, 7, 4,
+                    3, 0, 1,
+                    2
+                },
+                new Pins(PinPosition.Down, PinPosition.Up)
+            ),
+            new Solution(
+                new List<string> {
+                    "11", "10", "11",
+                    "10", "11", "10",
+                    "12", "13", "12"
+                },
+                new List<int> {
+                    6, 3, 4,
+                    7, 8, 5
+                },
+                new Pins(PinPosition.Down, PinPosition.Middle)
+            ),
+            new Solution(
+                new List<string> {
+                    "10", "11", "11",
+                    "01", "13", "11",
+                    "12", "00", "13"
+                },
+                new List<int> {
+                    6, 3, 0,
+                    1, 4, 5,
+                    8
+                },
+                new Pins(PinPosition.Down, PinPosition.Down)
             )
         };
 
